119bb423cd632a20f76e243005afd86e5d8517aa
dr0er <ernestxxx123@gmail.com>
2022-09-04 17:14:16 +0200

    Add home component change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
-import HeroBanner from "../components/HeroBanner";
-import SearchExercises from "../components/SearchExercises";
+
 import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
