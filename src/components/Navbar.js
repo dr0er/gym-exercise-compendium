4968b26d79bb9493912735e12a5ed42f3ea78ce0
dr0er <ernestxxx123@gmail.com>
2022-09-04 17:16:47 +0200

    Add Detail.js logic && styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
+
 import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => (
