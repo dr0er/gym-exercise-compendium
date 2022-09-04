11e26c83295ed46dd26ab8c97d246aed5ecf33c4
dr0er <ernestxxx123@gmail.com>
2022-09-04 17:25:12 +0200

    Add API KEY change

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": "619de6384cmshc1c5cabe037a2b8p16734bjsn09d300575ca1",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
