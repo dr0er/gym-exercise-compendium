e92e397ff9717fc8de1252d5c3b15db1d14dcfec
dr0er <ernestxxx123@gmail.com>
2022-09-04 17:25:46 +0200

    Add structure change

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -17,6 +17,5 @@ export const youtubeOptions = {
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
-
   return data;
 };
